bfa7fb3be333860cf9727e5ce88f45f52a16d8fc
lobo <kundankundan1200@gmail.com>
2026-06-24 19:26:34 +0530

    final touch up

--- server/server.js
+++ server/server.js
@@ -13,42 +13,47 @@ const server = http.createServer(app)
 
 // Initilize socket.io server
 export const io = new Server(server, {
-    cors: {origin: "*"}
+    cors: { origin: "*" }
 })
 
 // Store online users
 export const userSocketMap = {}; //{userId: socketId}
 
 // Socket.io connection handler
-io.on("connection", (socket)=> {
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     // Emit online users to all connected client
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
+    socket.on("disconnect", () => {
         console.log("User Disconnected", userId);
         delete userSocketMap[userId]
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
-        
+
     })
-    
+
 })
 
 // Middleware setup
 app.use(cors());
-app.use(express.json({limit: "10mb"}));
+app.use(express.json({ limit: "10mb" }));
 
 // route setup
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 // connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+
+// export server for Vercel
+export default server;
